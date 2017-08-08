43e726cd0d904d57078fbc8b99af2110f1a8993d
Vincent Boutour <bob@vibioh.fr>
2017-08-08 22:07:04 +0200

    Adding more tests

--- request_test.go
+++ request_test.go
@@ -2,10 +2,36 @@ package httputils
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
+
+func TestAddAuthorization(t *testing.T) {
+	var tests = []struct {
+		authorization string
+	}{
+		{
+			``,
+		},
+		{
+			`admin`,
+		},
+	}
+
+	for _, test := range tests {
+		request := httptest.NewRequest(`GET`, `http://localhost`, nil)
+		addAuthorization(request, test.authorization)
+
+		if result := strings.Join(request.Header[`Authorization`], ``); result != test.authorization {
+			t.Errorf(`addAuthorization(%v) = %v, want %v`, test.authorization, result, test.authorization)
+		}
+	}
+}
 
 func TestGetBasicAuth(t *testing.T) {
 	var tests = []struct {
@@ -63,7 +89,49 @@ func TestReadBody(t *testing.T) {
 		}
 
 		if failed {
-			t.Errorf(`GetBasicAuth(%v) = (%v, %v), want %v`, test.body, result, err, test.want, test.wantErr)
+			t.Errorf(`ReadBody(%v) = (%v, %v), want (%v, %v)`, test.body, result, err, test.want, test.wantErr)
+		}
+	}
+}
+
+func TestGetBody(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, `Hello, test`)
+	}))
+	defer testServer.Close()
+
+	var tests = []struct {
+		url     string
+		want    string
+		wantErr error
+	}{
+		{
+			testServer.URL,
+			`Hello, test
+`,
+			nil,
+		},
+	}
+
+	var failed bool
+
+	for _, test := range tests {
+		result, err := GetBody(test.url, ``)
+
+		failed = false
+
+		if err == nil && test.wantErr != nil {
+			failed = true
+		} else if err != nil && test.wantErr == nil {
+			failed = true
+		} else if err != nil && err.Error() != test.wantErr.Error() {
+			failed = true
+		} else if string(result) != test.want {
+			failed = true
+		}
+
+		if failed {
+			t.Errorf(`GetBody(%v, '') = (%s, %v), want (%s, %v)`, test.url, result, err, test.want, test.wantErr)
 		}
 	}
 }
