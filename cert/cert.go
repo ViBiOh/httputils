804d87789228c1f93a093805a19c3345cdb8ae96
Vincent <ViBiOh@users.noreply.github.com>
2017-10-19 09:42:00 +0200

    Update cert.go

--- cert/cert.go
+++ cert/cert.go
@@ -10,7 +10,6 @@ import (
 	"encoding/pem"
 	"flag"
 	"fmt"
-	"log"
 	"math/big"
 	"net"
 	"net/http"
@@ -75,7 +74,6 @@ func strSliceContains(slice []string, search string) bool {
 // Largely inspired by https://golang.org/src/net/http/server.go
 func ListenAndServeTLS(server *http.Server) error {
 	if *tlsCertFile != `` {
-		log.Printf(`Using provided certificate %s`, *tlsCertFile)
 		return server.ListenAndServeTLS(*tlsCertFile, *tlsKeyFile)
 	}
 
