f3bf1a69571fbe17c10167fc0642b8f9ff0be1b5
Vincent Boutour <bob@vibioh.fr>
2017-11-05 20:28:57 +0100

    Adding prepare query for search

--- db/db.go
+++ db/db.go
@@ -27,6 +27,22 @@ func (a WhereInInt64) Value() (driver.Value, error) {
 	return strings.Join(ints, ","), nil
 }
 
+// PrepareFullTextSearch replace $INDEX param in query and expand words
+func PrepareFullTextSearch(query, search string, index int) (string, string) {
+	if search == `` {
+		return ``, ``
+	}
+
+	words := strings.Split(search, ` `)
+	transformedWords := make([]string, 0, len(words))
+
+	for _, word := range words {
+		transformedWords = append(transformedWords, word+`:*`)
+	}
+
+	return strings.Replace(query, `$INDEX`, fmt.Sprintf(`$%d`, index), -1), strings.Join(transformedWords, ` | `)
+}
+
 // Flags add flags for given prefix
 func Flags(prefix string) map[string]*string {
 	return map[string]*string{
