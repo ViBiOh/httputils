d9e777753ee5ba376805fcc97fe07d1d76d22dad
Vincent Boutour <bob@vibioh.fr>
2018-01-17 19:44:04 +0100

    Removing explicit variable declaration

--- uuid/uuid.go
+++ uuid/uuid.go
@@ -6,16 +6,15 @@ import (
 )
 
 // New generates random UUID according to RFC 4122
-func New() (uuid string, err error) {
+func New() (string, error) {
 	raw := make([]byte, 16)
-	_, err = rand.Read(raw)
-	if err != nil {
+
+	if _, err := rand.Read(raw); err != nil {
 		return ``, err
 	}
 
 	raw[8] = raw[8]&^0xc0 | 0x80
 	raw[6] = raw[6]&^0xf0 | 0x40
-	uuid = fmt.Sprintf(`%x-%x-%x-%x-%x`, raw[0:4], raw[4:6], raw[6:8], raw[8:10], raw[10:])
 
-	return
+	return fmt.Sprintf(`%x-%x-%x-%x-%x`, raw[0:4], raw[4:6], raw[6:8], raw[8:10], raw[10:]), nil
 }
