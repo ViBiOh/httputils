ddb82b61e5aea0d60bdfdb3b7a5fb247e756c63f
Vincent Boutour <bob@vibioh.fr>
2018-03-10 22:30:25 +0100

    Adding sha1 fingerprint tools

---
+++ tools/sha_test.go
@@ -0,0 +1,30 @@
+package tools
+
+import (
+	"testing"
+)
+
+func Test_Sha1(t *testing.T) {
+	var cases = []struct {
+		intention string
+		input     interface{}
+		want      string
+	}{
+		{
+			`should work with nil`,
+			nil,
+			`3a9bcf8af38962fe340baa717773bf285f95121a`,
+		},
+		{
+			`should work with string`,
+			`Hello world`,
+			`7b502c3a1f48c8609ae212cdfb639dee39673f5e`,
+		},
+	}
+
+	for _, testCase := range cases {
+		if result := Sha1(testCase.input); result != testCase.want {
+			t.Errorf("%s\nSha1(%+v) = %+v, want %+v", testCase.intention, testCase.input, result, testCase.want)
+		}
+	}
+}
