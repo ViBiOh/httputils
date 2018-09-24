8c25cca68eaf981be72e72841aef272d5ee8fb0c
Vincent Boutour <bob@vibioh.fr>
2018-09-24 22:42:33 +0200

    Adding GetID method

--- pkg/tools/request.go
+++ pkg/tools/request.go
@@ -1,8 +1,16 @@
 package tools
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 // IsRoot checks if current path is root (empty or only trailing slash)
 func IsRoot(r *http.Request) bool {
 	return r.URL.Path == `/` || r.URL.Path == ``
+}
+
+// GetID return ID of URL (first section between two slashes)
+func GetID(r *http.Request) string {
+	return strings.Split(strings.Trim(r.URL.Path, `/`), `/`)[0]
 }
--- pkg/tools/request_test.go
+++ pkg/tools/request_test.go
@@ -38,3 +38,37 @@ func Test_IsRoot(t *testing.T) {
 		}
 	}
 }
+
+func Test_GetID(t *testing.T) {
+	emptyRequest, _ := http.NewRequest(http.MethodGet, `/`, nil)
+	simpleRequest, _ := http.NewRequest(http.MethodGet, `/abc-1234`, nil)
+	complexRequest, _ := http.NewRequest(http.MethodGet, `/def-5678/links/`, nil)
+
+	var cases = []struct {
+		intention string
+		request   *http.Request
+		want      string
+	}{
+		{
+			`should work with empty URL`,
+			emptyRequest,
+			``,
+		},
+		{
+			`should work with simple ID URL`,
+			simpleRequest,
+			`abc-1234`,
+		},
+		{
+			`should work with complex ID URL`,
+			complexRequest,
+			`def-5678`,
+		},
+	}
+
+	for _, testCase := range cases {
+		if result := GetID(testCase.request); result != testCase.want {
+			t.Errorf("%s\ngetID(`%+v`) = %+v, want %+v", testCase.intention, testCase.request, result, testCase.want)
+		}
+	}
+}
