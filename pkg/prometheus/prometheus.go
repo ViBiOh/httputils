349c1decfa782a83b808365e28be8f40f804c7e7
Vincent Boutour <bob@vibioh.fr>
2018-10-18 10:32:41 +0200

    adding path configuration for prometheus

--- pkg/prometheus/prometheus.go
+++ pkg/prometheus/prometheus.go
@@ -1,9 +1,13 @@
 package prometheus
 
 import (
+	"flag"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/ViBiOh/httputils/pkg/model"
+	"github.com/ViBiOh/httputils/pkg/tools"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -11,16 +15,21 @@ var _ model.Middleware = &App{}
 
 // App stores informations
 type App struct {
+	path string
 }
 
 // NewApp creates new App from Flags' config
-func NewApp(_ map[string]*string) *App {
-	return &App{}
+func NewApp(config map[string]*string) *App {
+	return &App{
+		path: strings.TrimSpace(*config[`path`]),
+	}
 }
 
 // Flags adds flags for given prefix
-func Flags(_ string) map[string]*string {
-	return nil
+func Flags(prefix string) map[string]*string {
+	return map[string]*string{
+		`path`: flag.String(tools.ToCamel(fmt.Sprintf(`%sPath`, prefix)), `/metrics`, `[prometheus] Path for exposing metrics`),
+	}
 }
 
 // Handler for net/http
@@ -28,7 +37,7 @@ func (a App) Handler(next http.Handler) http.Handler {
 	prometheusHandler := promhttp.Handler()
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == `/metrics` {
+		if r.URL.Path == a.path {
 			prometheusHandler.ServeHTTP(w, r)
 		} else {
 			next.ServeHTTP(w, r)
