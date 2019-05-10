89b816c157511bf404bf0407609d9621f0a41e98
Vincent Boutour <bob@vibioh.fr>
2019-05-10 19:21:58 +0200

    Migrating to t.Run for some easy test

--- pkg/uuid/uuid_test.go
+++ pkg/uuid/uuid_test.go
@@ -20,22 +20,24 @@ func TestNew(t *testing.T) {
 	var failed bool
 
 	for _, testCase := range cases {
-		result, err := New()
+		t.Run(testCase.intention, func(t *testing.T) {
+			result, err := New()
 
-		failed = false
+			failed = false
 
-		if err == nil && testCase.wantErr != nil {
-			failed = true
-		} else if err != nil && testCase.wantErr == nil {
-			failed = true
-		} else if err != nil && err.Error() != testCase.wantErr.Error() {
-			failed = true
-		} else if len(result) != testCase.want {
-			failed = true
-		}
+			if err == nil && testCase.wantErr != nil {
+				failed = true
+			} else if err != nil && testCase.wantErr == nil {
+				failed = true
+			} else if err != nil && err.Error() != testCase.wantErr.Error() {
+				failed = true
+			} else if len(result) != testCase.want {
+				failed = true
+			}
 
-		if failed {
-			t.Errorf("%s\nNew() = (%+v, %+v), want (%+v, %+v)", testCase.intention, result, err, testCase.want, testCase.wantErr)
-		}
+			if failed {
+				t.Errorf("New() = (%+v, %+v), want (%+v, %+v)", result, err, testCase.want, testCase.wantErr)
+			}
+		})
 	}
 }
