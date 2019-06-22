6d5fd92ba72792ce53769048138de3f37ce06c39
Vincent Boutour <bob@vibioh.fr>
2019-06-22 21:28:38 +0200

    Trying another way for instrumenting code

--- pkg/prometheus/prometheus.go
+++ pkg/prometheus/prometheus.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/ViBiOh/httputils/pkg/model"
 	"github.com/ViBiOh/httputils/pkg/tools"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -44,13 +45,16 @@ func New(config Config) *App {
 
 // Handler for net/http
 func (a App) Handler(next http.Handler) http.Handler {
-	prometheusHandler := promhttp.Handler()
+	instrumentedHandler := promhttp.InstrumentMetricHandler(
+		prometheus.DefaultRegisterer, next,
+	)
+	prometheusHandler := promhttp.HandlerFor(prometheus.DefaultGatherer, promhttp.HandlerOpts{})
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == a.path {
 			prometheusHandler.ServeHTTP(w, r)
 		} else {
-			next.ServeHTTP(w, r)
+			instrumentedHandler.ServeHTTP(w, r)
 		}
 	})
 }
