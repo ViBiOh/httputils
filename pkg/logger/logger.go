e78c3f0154c8ea965ff1d5ba640e93d1c34de24d
Vincent Boutour <bob@vibioh.fr>
2019-07-28 13:57:29 +0200

    Simplifying logger interface

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -9,13 +9,6 @@ import (
 	"github.com/ViBiOh/httputils/pkg/errors"
 )
 
-// LogReporter describes a log reporter
-type LogReporter interface {
-	Info(string)
-	Warn(string)
-	Error(string)
-}
-
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile | log.LUTC)
 }
@@ -25,17 +18,8 @@ var (
 	warn = log.New(os.Stderr, "WARN  ", log.LstdFlags|log.Lshortfile|log.LUTC)
 	erro = log.New(os.Stderr, "ERROR ", log.LstdFlags|log.Lshortfile|log.LUTC)
 
-	reporters = make([]LogReporter, 0)
-	mutex     = sync.RWMutex{}
+	mutex = sync.RWMutex{}
 )
-
-// AddReporter add a LogReporter to the list
-func AddReporter(reporter LogReporter) {
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	reporters = append(reporters, reporter)
-}
 
 func output(l *log.Logger, format string, a ...interface{}) string {
 	content := fmt.Sprintf(format, a...)
@@ -48,38 +32,25 @@ func output(l *log.Logger, format string, a ...interface{}) string {
 
 // Info log info message
 func Info(format string, a ...interface{}) {
-	content := output(info, format, a...)
-
-	for _, reporter := range reporters {
-		reporter.Info(content)
-	}
+	output(info, format, a...)
 }
 
 // Warn log warning message
 func Warn(format string, a ...interface{}) {
-	content := output(warn, format, a...)
-
-	for _, reporter := range reporters {
-		reporter.Warn(content)
-	}
+	output(warn, format, a...)
 }
 
 // Error log error message
 func Error(format string, a ...interface{}) {
-	content := output(erro, format, a...)
-
-	for _, reporter := range reporters {
-		reporter.Error(content)
-	}
+	output(erro, format, a...)
 }
 
 // Fatal log error message and exit
-func Fatal(format string, a ...interface{}) {
-	content := output(erro, format, a...)
-
-	for _, reporter := range reporters {
-		reporter.Error(content)
+func Fatal(err error) {
+	if err == nil {
+		return
 	}
 
+	output(erro, "%#v", err)
 	os.Exit(1)
 }
