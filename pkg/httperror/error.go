0ecf319b671f63e2060fb0419ec4658e9b2b78fa
Vincent Boutour <bob@vibioh.fr>
2019-11-26 20:48:36 +0100

    Adding no cache for error page

--- pkg/httperror/error.go
+++ pkg/httperror/error.go
@@ -6,30 +6,35 @@ import (
 	"github.com/ViBiOh/httputils/v3/pkg/logger"
 )
 
+func httpError(w http.ResponseWriter, status int, payload string) {
+	w.Header().Set("Cache-Control", "no-cache")
+	http.Error(w, payload, status)
+}
+
 // BadRequest logs error and sets BadRequest status
 func BadRequest(w http.ResponseWriter, err error) {
-	logger.Warn("HTTP/400 %s: %s", err.Error(), err)
-	http.Error(w, err.Error(), http.StatusBadRequest)
+	logger.Warn("HTTP/400 %s: %#v", err.Error(), err)
+	httpError(w, http.StatusBadRequest, err.Error())
 }
 
 // Unauthorized logs error and sets Unauthorized status
 func Unauthorized(w http.ResponseWriter, err error) {
-	logger.Warn("HTTP/401 %s: %s", err.Error(), err)
-	http.Error(w, err.Error(), http.StatusUnauthorized)
+	logger.Warn("HTTP/401 %s: %#v", err.Error(), err)
+	httpError(w, http.StatusUnauthorized, err.Error())
 }
 
 // Forbidden sets Forbidden status
 func Forbidden(w http.ResponseWriter) {
-	http.Error(w, "⛔️", http.StatusForbidden)
+	httpError(w, http.StatusForbidden, "⛔️")
 }
 
 // NotFound sets NotFound status
 func NotFound(w http.ResponseWriter) {
-	http.Error(w, "¯\\_(ツ)_/¯", http.StatusNotFound)
+	httpError(w, http.StatusNotFound, "¯\\_(ツ)_/¯")
 }
 
 // InternalServerError logs error and sets InternalServerError status
 func InternalServerError(w http.ResponseWriter, err error) {
-	logger.Error("HTTP/500 %s: %s", err.Error(), err)
-	http.Error(w, err.Error(), http.StatusInternalServerError)
+	logger.Error("HTTP/500 %s: %#v", err.Error(), err)
+	httpError(w, http.StatusInternalServerError, err.Error())
 }
