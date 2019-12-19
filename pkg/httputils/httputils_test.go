fff09f752851d85044c3fe4f274bdb0134096ebc
Vincent Boutour <bob@vibioh.fr>
2019-12-19 21:59:59 +0100

    test(httputils): Starting to add test for ChainMiddleware

--- pkg/httputils/httputils_test.go
+++ pkg/httputils/httputils_test.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/ViBiOh/httputils/v3/pkg/model"
 	"github.com/ViBiOh/httputils/v3/pkg/request"
 )
 
@@ -120,6 +121,52 @@ func TestHealthHandler(t *testing.T) {
 
 			if result, _ := request.ReadBodyResponse(writer.Result()); string(result) != testCase.want {
 				t.Errorf("HealthHandler = `%s`, want `%s`", string(result), testCase.want)
+			}
+		})
+	}
+}
+
+func TestChainMiddlewares(t *testing.T) {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("handler"))
+	})
+
+	var cases = []struct {
+		intention   string
+		request     *http.Request
+		middlewares []model.Middleware
+		want        string
+		wantStatus  int
+		wantHeader  http.Header
+	}{
+		{
+			"nil chain",
+			httptest.NewRequest(http.MethodGet, "/", nil),
+			nil,
+			"handler",
+			http.StatusOK,
+			http.Header{},
+		},
+	}
+
+	for _, testCase := range cases {
+		t.Run(testCase.intention, func(t *testing.T) {
+			writer := httptest.NewRecorder()
+			ChainMiddlewares(handler, testCase.middlewares...).ServeHTTP(writer, testCase.request)
+
+			if result := writer.Code; result != testCase.wantStatus {
+				t.Errorf("ChainMiddlewares = %d, want %d", result, testCase.wantStatus)
+			}
+
+			if result, _ := request.ReadBodyResponse(writer.Result()); string(result) != testCase.want {
+				t.Errorf("ChainMiddlewares = `%s`, want `%s`", string(result), testCase.want)
+			}
+
+			for key := range testCase.wantHeader {
+				want := testCase.wantHeader.Get(key)
+				if result := writer.Header().Get(key); result != want {
+					t.Errorf("%s Header = `%s`, want `%s`", key, result, want)
+				}
 			}
 		})
 	}
