17c466497cda927a11df8b25a8bc37be6ae46dcf
Vincent Boutour <bob@vibioh.fr>
2019-12-21 16:46:34 +0100

    feat(gzip): Removing gzipHandler middleware as already compatible

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/ViBiOh/httputils/v3
 go 1.13
 
 require (
-	github.com/NYTimes/gziphandler v1.1.1
 	github.com/lib/pq v1.2.0
 	github.com/prometheus/client_golang v1.2.1
 	github.com/tdewolff/minify/v2 v2.6.1
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -6,43 +6,49 @@ import (
 	"os"
 )
 
-func init() {
-	log.SetFlags(log.LstdFlags | log.Lshortfile | log.LUTC)
-}
+const (
+	callDepth = 3
+)
 
 var (
+	exitFunc = os.Exit
+
 	info = log.New(os.Stdout, "INFO  ", log.LstdFlags|log.Lshortfile|log.LUTC)
 	warn = log.New(os.Stderr, "WARN  ", log.LstdFlags|log.Lshortfile|log.LUTC)
 	erro = log.New(os.Stderr, "ERROR ", log.LstdFlags|log.Lshortfile|log.LUTC)
 )
 
-func output(l *log.Logger, format string, a ...interface{}) {
-	if err := l.Output(3, fmt.Sprintf(format, a...)); err != nil {
-		log.Printf("%s", err)
-	}
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile | log.LUTC)
 }
 
-// Info log info message
+// Info logs info message
 func Info(format string, a ...interface{}) {
 	output(info, format, a...)
 }
 
-// Warn log warning message
+// Warn logs warning message
 func Warn(format string, a ...interface{}) {
 	output(warn, format, a...)
 }
 
-// Error log error message
+// Error logs error message
 func Error(format string, a ...interface{}) {
 	output(erro, format, a...)
 }
 
-// Fatal log error message and exit
+// Fatal logs error message and exit with status code 1
 func Fatal(err error) {
 	if err == nil {
 		return
 	}
 
 	output(erro, "%s", err)
-	os.Exit(1)
+	exitFunc(1)
+}
+
+func output(l *log.Logger, format string, a ...interface{}) {
+	if err := l.Output(callDepth, fmt.Sprintf(format, a...)); err != nil {
+		log.Printf("%s", err)
+	}
 }
