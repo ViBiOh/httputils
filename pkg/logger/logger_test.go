6c52650913dca1d922f0ad6feb362a3a5bfa9094
Vincent Boutour <bob@vibioh.fr>
2020-09-21 22:09:54 +0200

    test: Improving coverage for logger package

--- pkg/logger/logger_test.go
+++ pkg/logger/logger_test.go
@@ -84,9 +84,9 @@ func TestStart(t *testing.T) {
 				level:  levelInfo,
 			},
 			args{
-				e: event{timestamp: time.Date(2020, 9, 30, 14, 59, 38, 0, time.UTC), level: levelWarning, message: "Hello world"},
+				e: event{timestamp: time.Date(2020, 9, 30, 14, 59, 38, 0, time.UTC), level: levelDebug, message: "Hello world"},
 			},
-			"2020-09-30T14:59:38Z WARNING Hello world\n",
+			"2020-09-30T14:59:38Z DEBUG Hello world\n",
 		},
 	}
 
@@ -108,9 +108,15 @@ func TestStart(t *testing.T) {
 	}
 }
 
-func TestText(t *testing.T) {
+func TestOutput(t *testing.T) {
 	type args struct {
-		e event
+		lev    level
+		format string
+		a      []interface{}
+	}
+
+	now = func() time.Time {
+		return time.Date(2020, 9, 21, 18, 34, 57, 0, time.UTC)
 	}
 
 	var cases = []struct {
@@ -119,22 +125,103 @@ func TestText(t *testing.T) {
 		want      string
 	}{
 		{
-			"simple",
+			"ignored",
 			args{
-				e: event{timestamp: time.Date(2020, 9, 30, 14, 59, 38, 0, time.UTC), level: levelInfo, message: "Hello world"},
+				lev:    levelDebug,
+				format: "Hello World",
 			},
-			"2020-09-30T14:59:38Z INFO Hello world\n",
+			"",
+		},
+		{
+			"info",
+			args{
+				lev:    levelInfo,
+				format: "Hello World",
+			},
+			"2020-09-21T18:34:57Z INFO Hello World\n",
+		},
+		{
+			"format",
+			args{
+				lev:    levelInfo,
+				format: "Hello %s",
+				a:      []interface{}{"World"},
+			},
+			"2020-09-21T18:34:57Z INFO Hello World\n",
 		},
 	}
 
 	for _, tc := range cases {
 		t.Run(tc.intention, func(t *testing.T) {
-			logger := Logger{}
+			writer := bytes.Buffer{}
+			logger := Logger{
+				outWriter: &writer,
+				level:     levelInfo,
+				buffer:    make(chan event, runtime.NumCPU()),
+			}
 
-			if got := logger.text(tc.args.e); string(got) != tc.want {
-				t.Errorf("text() = `%s`, want `%s`", got, tc.want)
+			logger.wg.Add(1)
+			go logger.Start()
+
+			logger.output(tc.args.lev, tc.args.format, tc.args.a...)
+			logger.Close()
+
+			got := writer.String()
+
+			if got != tc.want {
+				t.Errorf("output() = `%s`, want `%s`", got, tc.want)
 			}
 		})
+	}
+}
+
+func BenchmarkSimpleOutput(b *testing.B) {
+	logger := Logger{
+		outWriter: ioutil.Discard,
+		level:     levelInfo,
+		buffer:    make(chan event, runtime.NumCPU()),
+	}
+
+	logger.wg.Add(1)
+	go logger.Start()
+	defer logger.Close()
+
+	for i := 0; i < b.N; i++ {
+		logger.output(levelInfo, "Hello world")
+	}
+}
+
+func BenchmarkNoOutput(b *testing.B) {
+	logger := Logger{
+		outWriter: ioutil.Discard,
+		level:     levelWarning,
+		buffer:    make(chan event, runtime.NumCPU()),
+	}
+
+	logger.wg.Add(1)
+	go logger.Start()
+	defer logger.Close()
+
+	for i := 0; i < b.N; i++ {
+		logger.output(levelInfo, "Hello world")
+	}
+}
+
+func BenchmarkFormattedOutput(b *testing.B) {
+	logger := Logger{
+		outWriter: ioutil.Discard,
+		level:     levelInfo,
+		buffer:    make(chan event, runtime.NumCPU()),
+	}
+
+	logger.wg.Add(1)
+	go logger.Start()
+	defer logger.Close()
+
+	time := time.Now().Unix()
+
+	for i := 0; i < b.N; i++ {
+		logger.output(levelInfo, "Hello %s, it's %d", "Bob", time)
 	}
 }
 
@@ -191,6 +278,36 @@ func BenchmarkJson(b *testing.B) {
 	}
 }
 
+func TestText(t *testing.T) {
+	type args struct {
+		e event
+	}
+
+	var cases = []struct {
+		intention string
+		args      args
+		want      string
+	}{
+		{
+			"simple",
+			args{
+				e: event{timestamp: time.Date(2020, 9, 30, 14, 59, 38, 0, time.UTC), level: levelInfo, message: "Hello world"},
+			},
+			"2020-09-30T14:59:38Z INFO Hello world\n",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.intention, func(t *testing.T) {
+			logger := Logger{}
+
+			if got := logger.text(tc.args.e); string(got) != tc.want {
+				t.Errorf("text() = `%s`, want `%s`", got, tc.want)
+			}
+		})
+	}
+}
+
 func BenchmarkText(b *testing.B) {
 	logger := Logger{
 		outWriter: ioutil.Discard,
@@ -205,39 +322,5 @@ func BenchmarkText(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		logger.text(e)
-	}
-}
-
-func BenchmarkSimpleOutput(b *testing.B) {
-	logger := Logger{
-		outWriter: ioutil.Discard,
-		level:     levelInfo,
-		buffer:    make(chan event, runtime.NumCPU()),
-	}
-
-	logger.wg.Add(1)
-	go logger.Start()
-	defer logger.Close()
-
-	for i := 0; i < b.N; i++ {
-		logger.output(levelInfo, "Hello world")
-	}
-}
-
-func BenchmarkFormattedOutput(b *testing.B) {
-	logger := Logger{
-		outWriter: ioutil.Discard,
-		level:     levelInfo,
-		buffer:    make(chan event, runtime.NumCPU()),
-	}
-
-	logger.wg.Add(1)
-	go logger.Start()
-	defer logger.Close()
-
-	time := time.Now().Unix()
-
-	for i := 0; i < b.N; i++ {
-		logger.output(levelInfo, "Hello %s, it's %d", "Bob", time)
 	}
 }
