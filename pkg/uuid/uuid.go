58b506b711c3885f86fbcf2d46bcaa3fa9126d71
Vincent Boutour <bob@vibioh.fr>
2021-08-13 10:09:13 +0200

    feat(uuid): Adding comment explaining bitwise operation
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/uuid/uuid.go
+++ pkg/uuid/uuid.go
@@ -13,8 +13,8 @@ func New() (string, error) {
 		return "", fmt.Errorf("unable to read random: %s", err)
 	}
 
-	raw[8] = raw[8]&^0xc0 | 0x80
-	raw[6] = raw[6]&^0xf0 | 0x40
+	raw[6] = raw[6]&^0xf0 | 0x40 // set version to 4 (random uuid)
+	raw[8] = raw[8]&^0xc0 | 0x80 // set to IETF variant
 
 	return fmt.Sprintf("%x-%x-%x-%x-%x", raw[0:4], raw[4:6], raw[6:8], raw[8:10], raw[10:]), nil
 }
