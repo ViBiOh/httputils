9233d91483e0210cd47e5e85a8dda81c3abfbef8
Vincent Boutour <bob@vibioh.fr>
2021-09-29 11:49:37 +0200

    feat(redis): Adding metrics
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -3,16 +3,19 @@ package redis
 import (
 	"context"
 	"flag"
+	"fmt"
 	"time"
 
 	"github.com/ViBiOh/httputils/v4/pkg/flags"
-	"github.com/ViBiOh/httputils/v4/pkg/logger"
+	prom "github.com/ViBiOh/httputils/v4/pkg/prometheus"
 	"github.com/go-redis/redis/v8"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 // App of package
 type App struct {
 	redisClient *redis.Client
+	metrics     map[string]prometheus.Counter
 }
 
 // Config of package
@@ -32,14 +35,20 @@ func Flags(fs *flag.FlagSet, prefix string, overrides ...flags.Override) Config 
 }
 
 // New creates new App from Config
-func New(config Config) App {
+func New(config Config, prometheusRegisterer prometheus.Registerer) (App, error) {
+	metrics, err := prom.Counters(prometheusRegisterer, "redis", "", "store", "load", "delete", "exclusive", "error")
+	if err != nil {
+		return App{}, fmt.Errorf("unable to configure metrics: %s", err)
+	}
+
 	return App{
 		redisClient: redis.NewClient(&redis.Options{
 			Addr:     *config.redisAddress,
 			Password: *config.redisPassword,
 			DB:       *config.redisDatabase,
 		}),
-	}
+		metrics: metrics,
+	}, nil
 }
 
 // Ping check redis availability
@@ -49,33 +58,74 @@ func (a App) Ping() error {
 
 // Store store give key/val with duration
 func (a App) Store(ctx context.Context, key string, value interface{}, duration time.Duration) error {
-	return a.redisClient.SetEX(ctx, key, value, duration).Err()
+	err := a.redisClient.SetEX(ctx, key, value, duration).Err()
+
+	if err == nil {
+		a.increase("store")
+	} else {
+		a.increase("error")
+	}
+
+	return err
 }
 
 // Load given key
 func (a App) Load(ctx context.Context, key string) (string, error) {
-	return a.redisClient.Get(ctx, key).Result()
+	content, err := a.redisClient.Get(ctx, key).Result()
+
+	if err == nil {
+		a.increase("load")
+	} else if err != redis.Nil {
+		a.increase("error")
+	}
+
+	return content, err
 }
 
 // Delete given key
 func (a App) Delete(ctx context.Context, key string) error {
-	return a.redisClient.Del(ctx, key).Err()
+	err := a.redisClient.Del(ctx, key).Err()
+
+	if err == nil {
+		a.increase("delete")
+	} else {
+		a.increase("error")
+	}
+
+	return err
 }
 
 // Exclusive get an exclusive lock for given name during duration
 func (a App) Exclusive(ctx context.Context, name string, timeout time.Duration, action func(context.Context) error) error {
-	if !a.redisClient.SetNX(ctx, name, "acquired", timeout).Val() {
+	a.increase("exclusive")
+
+	if acquired, err := a.redisClient.SetNX(ctx, name, "acquired", timeout).Result(); err != nil {
+		a.increase("error")
+		return fmt.Errorf("unable to check semaphore: %s", err)
+	} else if !acquired {
 		return nil
 	}
-
-	defer func() {
-		if err := a.redisClient.Del(ctx, name).Err(); err != nil {
-			logger.WithField("name", name).Warn("unable to release exclusive lock: %s", err)
-		}
-	}()
 
 	actionCtx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
-	return action(actionCtx)
+	err := action(actionCtx)
+
+	if delErr := a.redisClient.Del(ctx, name).Err(); delErr != nil {
+		a.increase("error")
+
+		if err == nil {
+			err = delErr
+		} else {
+			err = fmt.Errorf("%s: %w", err, delErr)
+		}
+	}
+
+	return err
+}
+
+func (a App) increase(name string) {
+	if gauge, ok := a.metrics[name]; ok {
+		gauge.Inc()
+	}
 }
