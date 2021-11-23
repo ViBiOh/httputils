78c6383afde189f35f0989702c40e99c6a492cfa
Vincent Boutour <bob@vibioh.fr>
2021-11-23 11:47:30 +0100

    feat(concurrent): Adding concurrent work pool with fail-fast approach
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

---
+++ pkg/concurrent/concurrent.go
@@ -0,0 +1,81 @@
+package concurrent
+
+import (
+	"context"
+	"sync"
+)
+
+// Group describes a task group with a fail-fast approach
+type Group struct {
+	err     error
+	done    chan struct{}
+	limiter chan bool
+	cancel  context.CancelFunc
+	once    sync.Once
+	wg      sync.WaitGroup
+}
+
+// NewGroup creates a Group with given concurrency limit
+func NewGroup(limit uint64) *Group {
+	return &Group{
+		done:    make(chan struct{}),
+		limiter: make(chan bool, limit),
+	}
+}
+
+// WithContext make the given context cancelable for the group
+func (g *Group) WithContext(ctx context.Context) context.Context {
+	if g.cancel != nil {
+		panic("cancelable context already set-up")
+	}
+
+	ctx, g.cancel = context.WithCancel(ctx)
+	return ctx
+}
+
+// Go run given function in a goroutine according to limiter and current status
+func (g *Group) Go(f func() error) {
+	select {
+	case <-g.done:
+	case g.limiter <- true:
+		g.wg.Add(1)
+
+		go func() {
+			defer g.wg.Done()
+			if g.limiter != nil {
+				defer func() { <-g.limiter }()
+			}
+
+			if err := f(); err != nil {
+				g.close(err)
+			}
+		}()
+	}
+}
+
+// Wait for Group to end
+func (g *Group) Wait() error {
+	g.wg.Wait()
+
+	select {
+	case <-g.done:
+	default:
+		close(g.done)
+	}
+
+	close(g.limiter)
+
+	return g.err
+}
+
+func (g *Group) close(err error) {
+	g.once.Do(func() {
+		close(g.done)
+
+		if g.cancel != nil {
+			g.cancel()
+		}
+
+		g.err = err
+	})
+}
