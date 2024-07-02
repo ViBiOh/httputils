607bda54e5b0d17beb91916f593339ac6979e7b1
Vincent Boutour <bob@vibioh.fr>
2024-07-02 14:18:05 +0200

    fix(redis): Don't log error when redis client is closed
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/redis/pubsub.go
+++ pkg/redis/pubsub.go
@@ -3,6 +3,7 @@ package redis
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log/slog"
 
@@ -36,7 +37,7 @@ func (s *Service) Subscribe(ctx context.Context, channel string) (<-chan *redis.
 	pubsub := s.client.Subscribe(ctx, channel)
 
 	return pubsub.Channel(), func(ctx context.Context) {
-		if err := pubsub.Unsubscribe(ctx, channel); err != nil {
+		if err := pubsub.Unsubscribe(ctx, channel); err != nil && errors.Is(err, redis.ErrClosed) {
 			slog.LogAttrs(ctx, slog.LevelError, "unsubscribe pubsub", slog.String("channel", channel), slog.Any("error", err))
 		}
 
